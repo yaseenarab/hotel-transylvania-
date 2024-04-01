cafa9a477008b0bb83c894931742ae0889091be6
MitchThompson2001 <116986140+MitchThompson2001@users.noreply.github.com>
2024-04-01 13:14:11 -0500

    Add files via upload

--- RoomStatus.java
+++ RoomStatus.java
@@ -1,8 +1,12 @@
-package RoomEnums;
+package ReservationSystem.RoomEnums;
 
 //Vacant Clean, Vacant Dirty, Occupied Clean, Occupied Dirty, Out Of Order
 public enum RoomStatus {
     VaCl, VaDi,
     OcCl, OcDi,
-    OOO
+    OOO;
+    private static final RoomStatus[] types = values();
+    public RoomStatus next() {
+        return types[(this.ordinal() + 1) % types.length];
+    }
 }
